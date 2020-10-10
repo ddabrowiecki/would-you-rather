829db2c1c9cda8f664ea24a0f8636b3fb0b248a5
Dominic Dabrowiecki <ddabrowiecki@yahoo.com>
2020-10-09 17:59:48 -0700

    Add styling to question details page

--- src/components/QuestionDetails.js
+++ src/components/QuestionDetails.js
@@ -3,22 +3,59 @@ import { connect } from "react-redux";
 
 class QuestionDetails extends Component {
   render() {
-    const { id } = this.props;
+    const { question, authedUser, users } = this.props;
+    const id = question.id;
+    const answers = users[authedUser].answers;
+    const answerChoice = answers[id];
+    const answerText = question[answerChoice];
     return (
-      <div className="question-details">
-        <p>Question Details</p>
-        <p>Option: {id}</p>
+      <div className="question-list">
+        <div className="question-container">
+          <div className="user-header">
+            <p>{users[question.author].name} asks: </p>
+          </div>
+          <div className="question-box">
+            <div className="user-avatar">
+              <img
+                src={users[question.author].avatarURL}
+                className="user-image"
+                alt="None found"
+              ></img>
+            </div>
+            {answerChoice ? (
+              <div className="question-options">
+                <p className="p-header">Would You Rather...</p>
+                <div className="options">{question.optionOne.text}</div>
+                <div className="options">{question.optionTwo.text}</div>
+                <div className="p-header options-answer">
+                  You said: {answerText.text}
+                </div>
+              </div>
+            ) : (
+              <div className="question-options">
+                <p className="p-header">Would You Rather...</p>
+                <div className="options">{question.optionOne.text}</div>
+                <div className="options">{question.optionTwo.text}</div>
+                <div className="p-header options-answer">
+                  You haven't answered this question yet!
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-function mapStateToProps({ questions }, { match }) {
+function mapStateToProps({ authedUser, questions, users }, { match }) {
   const { id } = match.params;
   const question = questions[id];
   return {
     question,
     id,
+    authedUser,
+    users,
   };
 }
 
